fef9f88f113fe29cb1e0aca6810d3d0505833adb
Christoph Sax <christoph.sax@gmail.com>
2019-12-07 13:29:50 +0100

    Create lesson10.R

---
+++ lesson10.R
@@ -0,0 +1,24 @@
+#' ## Assignment Set 3: Lesson 10
+#'
+#' Add R Code under the questions.
+#' If you are done, 'knit' the document, commit and push to GitHub.
+#' You can use GitHub Pages if you like, add links to README.md
+#' to make it easier to find the pretty version.
+#'
+#' 1. Estimate an automated ARIMA model of Swiss GDP.
+#'
+
+#' 2. Inspect the model: What is the degree of seasonal and non-seasonal
+#' differentiation, what are the seasonal and non-seasonal AR and MA orders?
+#'
+
+#' 3. Re-estimate 1, using R base `arima()` function
+#'
+
+#' 4. Seasonally adjust Swiss GDP. Compare the ARIMA model with the one from
+#' `auto.arima`.
+#'
+
+#' 5. Download your stock title of choice. Use prophet to forecast the series.
+#'
+
